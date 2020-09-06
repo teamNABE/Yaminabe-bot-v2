a18aa71d24b30f38ef8f79c8fb5816e802416fde
huda0209 <huda030209@gmail.com>
2020-09-06 20:03:07 +0900

    [add]ownerGiveクラスを追加

--- class/msgEvent.js
+++ class/msgEvent.js
@@ -4,9 +4,11 @@ created by huda0209
 Yaminabe-bot v2 for discord bot 
 
 main.js :MAIN  'MAIN CODE'
- -msgEvent.js :CLASS  'liten some event and sort the task'
- -kick.js :CLASS  'kick the member'  <= this
+ -msgEvent.js :CLASS  'liten some event and sort the task'  <= this
+ -kick.js :CLASS  'kick the member'
  -ban.js :CLASS  'ban the member'
+ -announce_new_member.js :CLASS  'announce new member'
+ -ownerGive.js :CLASS  'give the owner role'
  
 ran by node.js
 
@@ -14,15 +16,16 @@ ran by node.js
 
 */
 
-const kickEvent = require('./kick.js')
-const banEvent = require('./ban.js')
+const kickEvent = require('./kick.js');
+const banEvent = require('./ban.js');
+const ownerGive = require('./ownerGive.js');
 
 
 class msgEvent {
     constructor(client,message,json) {
         this.client = client;
         this.message = message;
-        this.json = json
+        this.json = json;
     }
 
     async msgEvent ([command, ...args]){
@@ -30,20 +33,22 @@ class msgEvent {
         const message = this.message;
         const json = this.json;
 
-        const kicke = new kickEvent(client,message,json)
-        const bane = new banEvent(client,message,json)
+        const kicke = new kickEvent(client,message,json);
+        const bane = new banEvent(client,message,json);
+        const ownerGivee = new ownerGive(client,message,json);
 
 
         switch(command){
             case "kick" :
-                kicke.kick([command, ...args])
+                kicke.kick([command, ...args]);
                 break;
 
             case "ban" :
-                bane.ban([command, ...args])
+                bane.ban([command, ...args]);
                 break;
 
             case "owner" :
+                ownerGivee.ownerGive([command, ...args]);
                 break;
 
             case "reloadpanel" :
