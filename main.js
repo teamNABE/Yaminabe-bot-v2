f0a45a55dcefe37db7870b7fde8e1ff191de1cc2
huda0209 <huda030209@gmail.com>
2020-10-30 23:26:59 +0900

    [change]読み込みファイル名変更 その他変更多数

--- main.js
+++ main.js
@@ -13,54 +13,57 @@ main.js :MAIN  'MAIN CODE'　<= this
  
 ran by node.js
 
-2020-9-6
+2020-10-30
 
 */
 
 //node.js modules
-const dotenv = require('dotenv').config();
 const fs = require('fs');
 const discord = require("discord.js");
 
 //class
-const msgEvent = require('./class/msgEvent.js')
-const anmEvent = require('./class/announce_new_member.js')
+const msgEvent = require('./class/msgEvent.js');
+const anmEvent = require('./class/announce_new_member.js');
+
+//config
+let guildData = require('./config/guild/guildData.json');
+const BOT_DATA = require('./config/setting.json');
 
 //other 
 const option = {ws: {intents: discord.Intents.ALL}, restTimeOffset: 10};
 var json = JSON.parse(fs.readFileSync('./config/setting.json','utf8'));
+
 const client = new discord.Client(option);
-const letter = [[":zero:","0⃣"],[":one:","1⃣"],[":two:","2⃣"],[":three:","3⃣"],[":four:","4⃣"],[":five:","5⃣"],[":six:","6⃣"],[":seven:","7⃣"],[":eight:","8⃣"],[":nine:","9⃣"],[":keycap_ten:","🔟"],[":regional_indicator_a:","🇦"],[":regional_indicator_b:","🇧"],[":regional_indicator_c:","🇨"],[":regional_indicator_d:","🇩"],[":regional_indicator_e:","🇪"],[":regional_indicator_f:","🇫"],[":regional_indicator_g:","🇬"],[":regional_indicator_h:","🇭"],[":regional_indicator_i:","🇮"],[":regional_indicator_j:","🇯"],[":regional_indicator_k:","🇰"],[":regional_indicator_l:","🇱"],[":regional_indicator_m:","🇲"],[":regional_indicator_n:","🇳"],[":regional_indicator_o:","🇴"],[":regional_indicator_p:","🇵"],[":regional_indicator_q:","🇶"],[":regional_indicator_r:","🇷"],[":regional_indicator_s:","🇸"],[":regional_indicator_t:","🇹"],[":regional_indicator_u:","🇺"],[":regional_indicator_v:","🇻"],[":regional_indicator_w:","🇼"],[":regional_indicator_x:","🇽"],[":regional_indicator_y:","🇾"],[":regional_indicator_z:","🇿"]]
+const logger = require('./src/util/logger');
+const configChecker = require('./src/util/config');
 
 
 //start the bot
 client.on("ready", message => {
-  console.log(`bot is ready! ver: test \nlogin: ${client.user.tag}`);
+  logger.info(`bot is ready! ver. ${BOT_DATA.VERSION} \n        login: {cyan}${client.user.tag}\n`);
   //client.user.setActivity('Yaminabe-bot v2 Ver 2.1.1', { type: 'PLAYING' })
   //client.user.setActivity(process.env.ver, { type: 'PLAYING' })
-  client.channels.cache.get(json.guild.OperationChannel.Gamerole).messages.fetch(json.guild.MessageId[1][1]);
-  client.channels.cache.get(json.guild.OperationChannel.Compass).messages.fetch(json.guild.MessageId[0][1]);
+  client.channels.cache.get(guildData.guild.OperationChannel.Gamerole).messages.fetch(guildData.guild.MessageId[1][1]);
+  client.channels.cache.get(guildData.guild.OperationChannel.Compass).messages.fetch(guildData.guild.MessageId[0][1]);
 });
 
 //guild update event
 client.on("guildUpdate", bot =>{
-  json.guild.GuildName = bot.members.guild.name;
-  fs.writeFileSync('./config/setting.json',JSON.stringify(json),'utf8');
+  guildData.guild.GuildName = bot.members.guild.name;
+  fs.writeFileSync('./config/guild/guildData.json',JSON.stringify(guildData),'utf8');
   console.log("guildUpdate catch");
 })
 
 //message event
 client.on("message", async message => {
 
-  const prefix = '//'
-  if (message.content.startsWith(prefix)){
-    const [command, ...args] = message.content.slice(prefix.length).split(' ')
-    if(command === "stop"){
-      if(message.author.id === json.guild.Owner || message.member.roles.cache.get(json.guild.Role.top)){
-        console.log(`server stop. by${message.author.tag}`);
-        await message.delete()
-        process.exit(0);}
-    }
+  if (message.content.startsWith(BOT_DATA.PREFIX)){
+    const [command, ...args] = message.content.slice(BOT_DATA.PREFIX.length).split(' ');
+    if(command.toLowerCase() === "stop" &&(message.author.id === message.guild.ownerID || guildData.guild.Admin.indexOf(message.author.id)>-1)){
+        logger.info(`server was stoped by {cyan}${message.author.tag}`);
+        await message.delete();
+        client.destroy();
+        process.exit(0)};
 
     const msge = new msgEvent(client,message,json)
     msge.msgEvent([command, ...args])
@@ -91,27 +94,22 @@ client.on("messageReactionAdd", async(messageReaction ,user) =>{
 })    
 */
 
-if(process.env.DISCORD_BOT_TOKEN == undefined || process.env.DISCORD_BOT_TOKEN == ""){
-  console.log("please set ENV : DISCORD_BOT_TOKEN");
-  process.exit(0);
-}
 
-var token;
+configChecker.check(BOT_DATA);
+let token;
 if(process.argv.length>=3){
   switch(process.argv[2]){
     case "main" :
-      token = process.env.DISCORD_BOT_TOKEN;
+      token = BOT_DATA.MAIN_TOKEN;
       break;
-    case "test" :
-      if(process.env.TEST_TOKEN == undefined || process.env.TEST_TOKEN == ""){
-        console.log("please set ENV : TEST_TOKEN");
-        process.exit(0);
-      }
-      token = process.env.TEST_TOKEN;
+    case "div" :
+      configChecker.divCheck(BOT_DATA);
+      token = BOT_DATA.DIV_TOKEN;
+      BOT_DATA.VERSION = `dev(${BOT_DATA.VERSION})`;
       break;
     default :
-      console.log(`\nUnknown command. \nUsage \n node main.js main : main token \n node main.js test : test token`)
+      logger.error(`Unknown command. \nUsage \n {green}node main.js main{reset} : use main token \n {green}node main.js div{reset} : use divelopment token`);
       process.exit(0);
   };
-}else token = process.env.DISCORD_BOT_TOKEN
+}else token = BOT_DATA.MAIN_TOKEN;
 client.login(token);
