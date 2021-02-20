e7d51140984fdc2cb05e8d42d29f98cc8c9f3d8a
huda0209 <65341393+huda0209@users.noreply.github.com>
2021-02-20 21:33:44 +0900

    [fix/add] 特定リアクションのみに反応するように修正 メッセージ送信実行条件を修正 他 ロールのlengthより多い場合、実行しない 絵文字の配列にない場合は実行しない メッセージ送信を関数化し、ifブロック内で実行 memberがfalsyな値かチェックする機能追加

--- src/event/reaction.js
+++ src/event/reaction.js
@@ -13,7 +13,7 @@ main.js :MAIN  'MAIN CODE'
  
 ran by node.js
 
-2021-2-18
+2021-2-20
 
 */
 
@@ -27,59 +27,62 @@ async function reactionAdd(messageReaction,user,guildData){
 	const indexId = sign.indexOf(messageReaction.emoji.name);
 	const member = messageReaction.message.guild.members.cache.get(user.id);
 	let roleName;
+
+	if(indexId == -1) return;
+	if(!member) return;
 	
-	if(messageReaction.message.id == guildData.MessageId.CompassPanel){
+	if(messageReaction.message.id == guildData.MessageId.CompassPanel && indexId < guildData.CompassRank.length){
 		await member.roles.add(guildData.CompassRank[indexId][1]);
 		roleName = guildData.CompassRank[indexId][0];
+		reply(rolename,messageReaction,"付与");
 	};
 
-	if(messageReaction.message.id == guildData.MessageId.GamerolePanel){
+	if(messageReaction.message.id == guildData.MessageId.GamerolePanel && indexId < guildData.Gamerole.length){
 		await member.roles.add(guildData.Gamerole[indexId][1]);
 		roleName = guildData.Gamerole[indexId][0];
+		reply(rolename,messageReaction,"付与");
 	};
-	const reply = await messageReaction.message.channel.send({
-		embed: {
-		  description: `${roleName}の付与に成功しました。`,
-		  footer: {
-				  text: messageReaction.message.client.user.username,
-				  icon_url: messageReaction.message.client.user.avatarURL()
-				},
-		  color: guildData.Color.succeeded,
-		}
-	  });
-  	reply.delete({ timeout: 5000 });
 }
 
 async function reactionRemove(messageReaction,user,guildData){
-
 	if(user.bot) return;
 	if(messageReaction.message.guild.id != guildData.Server) return;
 
 	const indexId = sign.indexOf(messageReaction.emoji.name);
 	const member = messageReaction.message.guild.members.cache.get(user.id);
 	let roleName;
-	
-	if(messageReaction.message.id == guildData.MessageId.CompassPanel){
+
+	if(indexId == -1) return;
+	if(!member) return;
+		
+	if(messageReaction.message.id == guildData.MessageId.CompassPanel && indexId < guildData.CompassRank.length){
 		await member.roles.remove(guildData.CompassRank[indexId][1]);
 		roleName = guildData.CompassRank[indexId][0];
+		reply(rolename,messageReaction,"削除");
 	};
 
-	if(messageReaction.message.id == guildData.MessageId.GamerolePanel){
+	if(messageReaction.message.id == guildData.MessageId.GamerolePanel && indexId < guildData.Gamerole.length){
 		await member.roles.remove(guildData.Gamerole[indexId][1]);
 		roleName = guildData.Gamerole[indexId][0];
+		reply(rolename,messageReaction,"削除");
 	};
-	const reply = await messageReaction.message.channel.send({
-		embed: {
-		  description: `${roleName}の削除に成功しました。`,
-		  footer: {
-				  text: messageReaction.message.client.user.username,
-				  icon_url: messageReaction.message.client.user.avatarURL()
-				},
-		  color: guildData.Color.succeeded,
-		}
-	  });
-  	reply.delete({ timeout: 5000 });
 };
+
+
+function reply(roleName,messageReaction,processName){
+	const reply = await messageReaction.message.channel.send({
+			embed: {
+			  description: `${roleName}の${processName}に成功しました。`,
+			  footer: {
+					  text: messageReaction.message.client.user.username,
+					  icon_url: messageReaction.message.client.user.avatarURL()
+					},
+			  color: guildData.Color.succeeded,
+			}
+		});
+	reply.delete({ timeout: 5000 });
+}
+
 
 exports.reactionAdd = reactionAdd;
 exports.reactionRemove = reactionRemove;
