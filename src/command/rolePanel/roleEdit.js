5f2e02cfae97ee205b1384d4a58b6be4e9051610
huda0209 <65341393+huda0209@users.noreply.github.com>
2021-02-26 23:09:51 +0900

    [add]loggerを追加

--- src/command/rolePanel/roleEdit.js
+++ src/command/rolePanel/roleEdit.js
@@ -22,10 +22,9 @@ ran by node.js
 const fs = require("fs");
 const logger = require("../../util/logger");
 
-async function roleEditCommandHandler([command, ...args],message,guildData,BOT_DATA){
-
-    if(!message.member._roles.indexOf(guildData.Roles.Nabe)>-1)
-        return await message.channel.send({
+async function roleEditCommandHandler([command, ...args],message,guildData){
+    if(message.member._roles.indexOf(guildData.Roles.Nabe) == -1){
+        await message.channel.send({
             embed: {
                 title: "実行する権限がありません。",
                 color: guildData.Color.failed,
@@ -36,26 +35,30 @@ async function roleEditCommandHandler([command, ...args],message,guildData,BOT_D
                 timestamp: new Date()
             }
         });
+        logger.info(`reserved "${command} ${args[0]} ${args[1]} ${args[2]}", but user didn't have permission.`);
+        return;
+};
 
     const mode = args[0].toLowerCase()=="gamerole"? "Gamerole" : "CompassRank";
     switch(args[1].toLowerCase()){
         case "add" :
-            roleAdd([command, ...args],message,mode,guildData,BOT_DATA);
+            roleAdd([command, ...args],message,mode,guildData);
             break;
 
         case "delete" : 
         case "del" :
-            roleDelete([command, ...args],message,mode,guildData,BOT_DATA)
+            roleDelete([command, ...args],message,mode,guildData)
             break;
 
         default :
-            
-        break;
-    }
-}
+            message.channel.send(`不明なコマンドです。`)
+            logger.info(`reserved "${command} ${args[0]} ${args[1]} ${args[2]}", but unknown command.`);
+            break;
+    };
+};
 
 
-async function roleAdd([command, ...args],message,mode,guildData,BOT_DATA){
+async function roleAdd([command, ...args],message,mode,guildData){
     const mentiodRole = message.mentions.roles.first();
     let role;
     
@@ -63,23 +66,27 @@ async function roleAdd([command, ...args],message,mode,guildData,BOT_DATA){
         role = message.guild.roles.cache.get(args[2]);
     }else{
         role = mentiodRole;
-    }
+    };
 
     if(!role){
         message.channel.send("有効なロールを指定してください。");
+        logger.info(`reserved "${command} ${args[0]} ${args[1]} ${args[2]}", but this role can't read.`);
         return;
-    }
+    };
     
     if(checkRole(role,mode,guildData)){
         message.channel.send(`そのロールは${mode}に追加済みです。`);
+        logger.info(`reserved "${command} ${args[0]} ${args[1]} ${args[2]}", but ${role.name} role already added.`);
         return;
-    }
-    guildData[mode].push([role.name,role.id]);
-    fs.writeFileSync('./config/guild/guildData.json', JSON.stringify(guildData, null, "\t"),'utf8')
-    message.channel.send(`${role.name}を${mode}に追加しました。`);
-}
+    };
 
-async function roleDelete([command, ...args],message,mode,guildData,BOT_DATA){
+    guildData[mode].push([role.name,role.id]);
+    fs.writeFileSync('./config/guild/guildData.json', JSON.stringify(guildData, null, "\t"),'utf8');
+    message.channel.send(`${role.name}を${mode}に追加しました。`);
+    logger.info(`reserved "${command} ${args[0]} ${args[1]} ${args[2]}". suceeded to add ${role.name} role.`);
+};
+
+async function roleDelete([command, ...args],message,mode,guildData){
     const mentiodRole = message.mentions.roles.first();
     let role;
     
@@ -87,24 +94,26 @@ async function roleDelete([command, ...args],message,mode,guildData,BOT_DATA){
         role = message.guild.roles.cache.get(args[2]);
     }else{
         role = mentiodRole;
-    }
-    console.log(role)
+    };
+    console.log(role);
     if(!role){
         message.channel.send("有効なロールを指定してください。");
+        logger.info(`reserved "${command} ${args[0]} ${args[1]} ${args[2]}", but this role can't read.`);
         return;
-    }
+    };
 
     if(!checkRole(role,mode,guildData)){
-        message.channel.send(`そのロールは${mode}に存在しません。`)
+        message.channel.send(`そのロールは${mode}に存在しません。`);
+        logger.info(`reserved "${command} ${args[0]} ${args[1]} ${args[2]}", but this role can't read.`);
         return;
-    }
+    };
 
     delete guildData[mode][guildData[mode].map(key => key[1]).indexOf(role.id)];
     guildData[mode] = guildData[mode].filter(Boolean);
 
     fs.writeFileSync('./config/guild/guildData.json', JSON.stringify(guildData, null, "\t"),'utf8');
     message.channel.send(`${role.name}を${mode}から削除しました。`);
-
+    logger.info(`reserved "${command} ${args[0]} ${args[1]} ${args[2]}". suceeded to delete ${role.name} role.`);
 }
 
 function checkRole(role,mode,guildData){
