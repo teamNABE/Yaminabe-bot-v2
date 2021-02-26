cb41e2fb0ca759e1009242796be5665f1ba71d06
huda0209 <65341393+huda0209@users.noreply.github.com>
2021-02-26 23:12:06 +0900

    [add]loggerを追加

--- src/event/announce_new_member.js
+++ src/event/announce_new_member.js
@@ -7,15 +7,18 @@ main.js :MAIN  'MAIN CODE'
  -command-handler.js :module
  -punish.js :module
  -ownerGive.js :module
- -rolePanel.js :module
+ -rolePanelReload.js :module
+ -roleEdit.js  :module
  -announce_new_member :module  <= this
  -reaction.js :module
  
 ran by node.js
 
-2021-2-18
+2021-2-26
 
 */
+const logger = require("../util/logger");
+
 async function announce_new_member(member,guildData){
     if(member.user.bot) return;
     let message = guildData.Message.Welcome;
@@ -25,6 +28,7 @@ async function announce_new_member(member,guildData){
     message = message.replace(/{SERVERINTRO_CH}/g, `<#${guildData.Channels.ServerIntroduction}>`);
     message = message.replace(/{SELFINTRO_CH}/g, `<#${guildData.Channels.SelfIntroduction}>`);
     await member.guild.channels.cache.get(guildData.Channels.Welcome).send(message);
-}
+    logger.info(`reserved "guildMemberAdd" Event. suceeded to send new member notice memssage.`);
+};
 
 exports.announce_new_member = announce_new_member;
