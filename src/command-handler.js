146fb64c2f74e86e184f1f3cba1279a0839846c7
huda0209 <65341393+huda0209@users.noreply.github.com>
2021-02-26 23:08:47 +0900

    [change]クライアントを引数ではなくメッセージから所得するように変更

--- src/command-handler.js
+++ src/command-handler.js
@@ -17,55 +17,57 @@ ran by node.js
 2021-2-26
 
 */
+
 const punish = require('./command/punish')
 const ownerGive = require('./command/ownerGive.js');
-const rolePanelEvent = require('./command/rolePanel.js');
+const rolePanelEvent = require('./command/rolePanel/rolePanelReload.js');
 const roleEdit = require("./command/rolePanel/roleEdit")
 const logger = require('./util/logger');
 
 
-async function commandHandler([command, ...args],message,guildData,BOT_DATA,client){
+async function commandHandler([command, ...args],message,guildData,BOT_DATA){
 
     switch(command.toLowerCase()){
         case BOT_DATA.COMMAND :
-            if(!(message.author.id == message.guild.ownerID || guildData.Admin.indexOf(message.author.id)>-1)) break;
-            AdminCommandHandler([command, ...args],message,guildData,BOT_DATA,client);
+            if(!(message.author.id == message.guild.ownerID || message.member._roles.indexOf(guildData.Roles.Admin)==-1)) break;
+            AdminCommandHandler([command, ...args],message,guildData);
             break;
 
         case "kick" :
         case "ban" :
-            punish.punish([command, ...args],message,guildData,BOT_DATA,client);
+            punish.punish([command, ...args],message,guildData);
             break;
       };
-}
+};
 
 
-async function AdminCommandHandler([command, ...args],message,guildData,BOT_DATA,client){
+async function AdminCommandHandler([command, ...args],message,guildData){
 
     switch(args[0].toLowerCase()){
         case "owner" :
-            ownerGive.ownerGive([command, ...args],message,guildData,BOT_DATA,client);
+            ownerGive.ownerGive([command, ...args],message,guildData);
             break;
     
         case "reloadpanel" :
-            rolePanelEvent.reloadPanel([command, ...args],message,guildData,BOT_DATA,client);
+            rolePanelEvent.reloadPanel([command, ...args],message,guildData);
             break;
 
         case "gamerole" :
         case "compassRank":
-            roleEdit.roleEditCommandHandler([command, ...args],message,guildData,BOT_DATA);
+            roleEdit.roleEditCommandHandler([command, ...args],message,guildData);
             break;
 
         case "stop" :
             logger.info(`server was stoped by {cyan}${message.author.tag}`);
             await message.delete();
-            client.destroy();
+            message.client.destroy();
             process.exit(0);
         
         default:
+            logger.info(`reserved ${args[0]} command, but unknown command.`);
             message.reply(`Unknown command.`);
             break;
       };
-}
+};
 
-exports.commandHandler = commandHandler
+exports.commandHandler = commandHandler;
