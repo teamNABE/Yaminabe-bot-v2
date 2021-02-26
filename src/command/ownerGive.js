8124e1e5f2e9405b65c7b4ce9995ec92e3d9790d
huda0209 <65341393+huda0209@users.noreply.github.com>
2021-02-26 23:09:19 +0900

    [fix/add]プロパティが違うミスを修正 loggerを追加

--- src/command/ownerGive.js
+++ src/command/ownerGive.js
@@ -7,24 +7,32 @@ main.js :MAIN  'MAIN CODE'
  -command-handler.js :module
  -punish.js :module
  -ownerGive.js :module  <= this
- -rolePanel.js :module
+ -rolePanelReload.js :module
+ -roleEdit.js  :module
  -announce_new_member :module
  -reaction.js :module
  
 ran by node.js
 
-2021-2-22
+2021-2-26
 
 */
+const fs = require("fs");
+const logger = require("../util/logger");
 
+async function ownerGive([command, ...args],message,guildData){
 
-async function ownerGive([command, ...args],message,guildData,BOT_DATA,client){
+		if(!(message.channel.id === guildData.OperationChannel.Emergency)){
+			await message.delete();
+			logger.info(`reserved "${command} ${args[0]} ${args[1]}", but this channel different.`);
+		};
+		if(!(args[1] == guildData.OwnerGive.password)){
+			await message.delete();
+			logger.info(`reserved "${command} ${args[0]} ${args[1]}", but wrong password.`);
+		};
 
-		if(!(message.channel.id === guildData.OperationChannel.Emergency)) return message.delete();
-		if(!(args[1] == guildData.ownerGive.password)) return message.delete();
-
-		if(!guildData.ownerGive.state){
-			message.guild.channels.cache.get(guildData.OperationChannel.Emergency).send({
+		if(!guildData.OwnerGive.state){
+			await message.guild.channels.cache.get(guildData.OperationChannel.Emergency).send({
 				embed: {
 					author: {
 						name: message.member.user.username,
@@ -33,19 +41,20 @@ async function ownerGive([command, ...args],message,guildData,BOT_DATA,client){
 				  	title: `This process was dinable. If you are Admin, check the config file.`,
 				  	color: guildData.Color.failed,
 				  	footer: {
-						text: client.user.username,
-						icon_url: client.user.avatarURL()},
+						text: message.client.user.username,
+						icon_url: message.client.user.avatarURL()},
 				  	timestamp: new Date()
 				}
 			});
+			logger.info(`reserved "${command} ${args[0]} ${args[1]}", but this function was disnable.`);
 			return;
-		}
+		};
 
-		message.member.roles.add(guildData.Roles.Owner);
-		message.delete();
-		guildData.ownerGive.state = false;
-		guildData.ownerGive.timestamp = new Date();
-		fs.writeFileSync('./config/guild/guildData.json',JSON.stringify(guildData),'utf8');
+		await message.member.roles.add(guildData.Roles.Owner);
+		await message.delete();
+		guildData.OwnerGive.state = false;
+		guildData.OwnerGive.timestamp = new Date();
+		fs.writeFileSync('./config/guild/guildData.json', JSON.stringify(guildData, null, "\t"),'utf8');
 
 
 		message.guild.channels.cache.get(guildData.OperationChannel.Emergency).send({
@@ -54,15 +63,16 @@ async function ownerGive([command, ...args],message,guildData,BOT_DATA,client){
 				icon_url: message.member.user.avatarURL() != null ? message.member.user.avatarURL() : message.member.user.defaultAvatarURL
 			},
 			embed: {
-				title: `succeeded to give the owner role.\nUSER : <@${message.user.id}>`,
+				title: `succeeded to give the owner role.\nUSER : <@!${message.member.user.id}>`,
 				color: guildData.Color.succeeded,
 				footer: {
-							text: client.user.username,
-							icon_url: client.user.avatarURL()},
+							text: message.client.user.username,
+							icon_url: message.client.user.avatarURL()},
 				timestamp: new Date()
 			}
 		});
-}       
+		logger.info(`reserved "${command} ${args[0]} ${args[1]} ${args[2]}". suceeded to give ${message.member.user.tag} owner role.`);
+};   
 
 
-exports.ownerGive = ownerGive
+exports.ownerGive = ownerGive;
